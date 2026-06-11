d1c24387761716b184e1ae1531c870de4a494258
albina demaj <demajalbina3@gmail.com>
2026-06-11 09:40:45 +0200

    Përshkrimi i ndryshimeve

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>MediaVault</title>
+    <title inertia>Cloud</title>
 
     <link rel="icon" type="image/png" href="/images/MediaValut.png">
 
